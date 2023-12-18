c3d94a3fadb1e2010632fbd9a1860f37b131e5c9
Daniel Valencia <valenciad1994@gmail.com>
2023-12-18 12:59:34 -0500

    updated header

--- src/pages/DocumentUpload/sections/Header.tsx
+++ src/pages/DocumentUpload/sections/Header.tsx
@@ -1,6 +1,11 @@
+import Image from 'next/image';
+
 export default function Header() {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center relative'>
+      <div className='absolute -left-12 -top-6'>
+        <Image src='/cancel.svg' alt='Close Button' width={30} height={30}/>
+      </div>
       <div className='p-2 border-b-2 border-gray-300'>
         <p className='text-2xl md:text-3xl text-blue-900 font-bold'>Document Upload</p>
       </div>
